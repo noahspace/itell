651e1922c39a61d5c82baafc048a66f28c6ce974
Noah Zhang <226582@qq.com>
2022-06-25 12:19:56 +0800

    数据库文件更新

--- itell.sql
+++ itell.sql
@@ -14,10 +14,10 @@ CREATE TABLE IF NOT EXISTS `options` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `options` (`name`, `value`) VALUES
-	('plugins', 'a:2:{s:11:"HelloWorld1";a:3:{s:10:"class_name";s:34:"Content\\Plugins\\HelloWorld1\\Plugin";s:6:"config";a:0:{}s:7:"handles";a:2:{s:22:"admin/header.php:brief";a:1:{i:0;s:42:"Content\\Plugins\\HelloWorld1\\Plugin::render";}s:14:"index.php:main";a:1:{i:0;s:40:"Content\\Plugins\\HelloWorld1\\Plugin::main";}}}s:10:"HelloWorld";a:3:{s:10:"class_name";s:33:"Content\\Plugins\\HelloWorld\\Plugin";s:6:"config";a:1:{s:5:"value";s:12:"Hello World!";}s:7:"handles";a:2:{s:22:"admin/header.php:brief";a:1:{i:0;s:41:"Content\\Plugins\\HelloWorld\\Plugin::render";}s:14:"index.php:main";a:1:{i:0;s:39:"Content\\Plugins\\HelloWorld\\Plugin::main";}}}}'),
+	('plugins', 'a:1:{s:10:"HelloWorld";a:3:{s:10:"class_name";s:33:"Content\\Plugins\\HelloWorld\\Plugin";s:6:"config";a:1:{s:5:"value";s:12:"Hello World!";}s:7:"handles";a:1:{s:32:"admin/header.php:adminHeaderUser";a:1:{i:0;s:41:"Content\\Plugins\\HelloWorld\\Plugin::render";}}}}'),
 	('rewrite', '0'),
 	('routingTable', 'a:2:{i:0;a:3:{s:4:"regx";s:9:"/^[\\/]?$/";s:6:"widget";s:19:"\\Itell\\Widget\\Index";s:6:"action";s:6:"render";}i:1;a:3:{s:4:"regx";s:35:"/^\\/action\\/([_0-9a-zA-Z-]+)[\\/]?$/";s:6:"widget";s:20:"\\Itell\\Widget\\Action";s:6:"params";a:1:{i:0;s:6:"action";}}}'),
-	('theme', 'a:2:{s:9:"activated";s:5:"itell";s:6:"config";a:0:{}}');
+	('theme', 'a:2:{s:9:"activated";s:7:"default";s:6:"config";a:2:{s:4:"val1";s:10:"默认值1";s:4:"val2";s:10:"默认值2";}}');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
